00d5df2d62a520d2717e96c487e7ff298a60660c
BPitts8019 <bradleyj.pitts@gmail.com>
2019-08-30 19:40:54 -0700

    Completed MVP

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -15,124 +15,125 @@ const App = () => {
    useEffect(() => {
       console.log("App has started...");
 
-      // let promises = [];
-      // for (let i = 1; i <= 3; i++) {
-      //    promises.push(axios.get(`https://swapi.co/api/people/${i}/`));
-      // }
+      let promises = [];
+      for (let i = 1; i <= 15; i++) {
+         promises.push(axios.get(`https://swapi.co/api/people/${i}/`));
+      }
 
-      // axios.all(promises)
-      //    .then(results => {
-      //       const starWarsChar = [];
+      axios.all(promises)
+         .then(results => {
+            const starWarsChar = [];
 
-      //       results.forEach(response => {starWarsChar.push(response.data)});
-      //       console.log(JSON.stringify(starWarsChar, null, 3));
-      //       // setCharacters(starWarsChar);
-      //    })
-      //    .catch(error => {
-      //       console.log("There was a problem retrieving the characters from SWAPI.");
-      //       console.error(error);
-      //    });
+            results.forEach(response => {starWarsChar.push(response.data)});
+            console.log(JSON.stringify(starWarsChar, null, 3));
+            setCharacters(starWarsChar);
+         })
+         .catch(error => {
+            console.log("There was a problem retrieving the characters from SWAPI.");
+            console.error(error);
+         });
 
-      setCharacters([
-         {
-            "name": "Luke Skywalker",
-            "height": "172",
-            "mass": "77",
-            "hair_color": "blond",
-            "skin_color": "fair",
-            "eye_color": "blue",
-            "birth_year": "19BBY",
-            "gender": "male",
-            "homeworld": "https://swapi.co/api/planets/1/",
-            "films": [
-               "https://swapi.co/api/films/2/",
-               "https://swapi.co/api/films/6/",
-               "https://swapi.co/api/films/3/",
-               "https://swapi.co/api/films/1/",
-               "https://swapi.co/api/films/7/"
-            ],
-            "species": [
-               "https://swapi.co/api/species/1/"
-            ],
-            "vehicles": [
-               "https://swapi.co/api/vehicles/14/",
-               "https://swapi.co/api/vehicles/30/"
-            ],
-            "starships": [
-               "https://swapi.co/api/starships/12/",
-               "https://swapi.co/api/starships/22/"
-            ],
-            "created": "2014-12-09T13:50:51.644000Z",
-            "edited": "2014-12-20T21:17:56.891000Z",
-            "url": "https://swapi.co/api/people/1/"
-         },
-         {
-            "name": "C-3PO",
-            "height": "167",
-            "mass": "75",
-            "hair_color": "n/a",
-            "skin_color": "gold",
-            "eye_color": "yellow",
-            "birth_year": "112BBY",
-            "gender": "n/a",
-            "homeworld": "https://swapi.co/api/planets/1/",
-            "films": [
-               "https://swapi.co/api/films/2/",
-               "https://swapi.co/api/films/5/",
-               "https://swapi.co/api/films/4/",
-               "https://swapi.co/api/films/6/",
-               "https://swapi.co/api/films/3/",
-               "https://swapi.co/api/films/1/"
-            ],
-            "species": [
-               "https://swapi.co/api/species/2/"
-            ],
-            "vehicles": [],
-            "starships": [],
-            "created": "2014-12-10T15:10:51.357000Z",
-            "edited": "2014-12-20T21:17:50.309000Z",
-            "url": "https://swapi.co/api/people/2/"
-         },
-         {
-            "name": "R2-D2",
-            "height": "96",
-            "mass": "32",
-            "hair_color": "n/a",
-            "skin_color": "white, blue",
-            "eye_color": "red",
-            "birth_year": "33BBY",
-            "gender": "n/a",
-            "homeworld": "https://swapi.co/api/planets/8/",
-            "films": [
-               "https://swapi.co/api/films/2/",
-               "https://swapi.co/api/films/5/",
-               "https://swapi.co/api/films/4/",
-               "https://swapi.co/api/films/6/",
-               "https://swapi.co/api/films/3/",
-               "https://swapi.co/api/films/1/",
-               "https://swapi.co/api/films/7/"
-            ],
-            "species": [
-               "https://swapi.co/api/species/2/"
-            ],
-            "vehicles": [],
-            "starships": [],
-            "created": "2014-12-10T15:11:50.376000Z",
-            "edited": "2014-12-20T21:17:50.311000Z",
-            "url": "https://swapi.co/api/people/3/"
-         }
-      ]);
+      // setCharacters([
+      //    {
+      //       "name": "Luke Skywalker",
+      //       "height": "172",
+      //       "mass": "77",
+      //       "hair_color": "blond",
+      //       "skin_color": "fair",
+      //       "eye_color": "blue",
+      //       "birth_year": "19BBY",
+      //       "gender": "male",
+      //       "homeworld": "https://swapi.co/api/planets/1/",
+      //       "films": [
+      //          "https://swapi.co/api/films/2/",
+      //          "https://swapi.co/api/films/6/",
+      //          "https://swapi.co/api/films/3/",
+      //          "https://swapi.co/api/films/1/",
+      //          "https://swapi.co/api/films/7/"
+      //       ],
+      //       "species": [
+      //          "https://swapi.co/api/species/1/"
+      //       ],
+      //       "vehicles": [
+      //          "https://swapi.co/api/vehicles/14/",
+      //          "https://swapi.co/api/vehicles/30/"
+      //       ],
+      //       "starships": [
+      //          "https://swapi.co/api/starships/12/",
+      //          "https://swapi.co/api/starships/22/"
+      //       ],
+      //       "created": "2014-12-09T13:50:51.644000Z",
+      //       "edited": "2014-12-20T21:17:56.891000Z",
+      //       "url": "https://swapi.co/api/people/1/"
+      //    },
+      //    {
+      //       "name": "C-3PO",
+      //       "height": "167",
+      //       "mass": "75",
+      //       "hair_color": "n/a",
+      //       "skin_color": "gold",
+      //       "eye_color": "yellow",
+      //       "birth_year": "112BBY",
+      //       "gender": "n/a",
+      //       "homeworld": "https://swapi.co/api/planets/1/",
+      //       "films": [
+      //          "https://swapi.co/api/films/2/",
+      //          "https://swapi.co/api/films/5/",
+      //          "https://swapi.co/api/films/4/",
+      //          "https://swapi.co/api/films/6/",
+      //          "https://swapi.co/api/films/3/",
+      //          "https://swapi.co/api/films/1/"
+      //       ],
+      //       "species": [
+      //          "https://swapi.co/api/species/2/"
+      //       ],
+      //       "vehicles": [],
+      //       "starships": [],
+      //       "created": "2014-12-10T15:10:51.357000Z",
+      //       "edited": "2014-12-20T21:17:50.309000Z",
+      //       "url": "https://swapi.co/api/people/2/"
+      //    },
+      //    {
+      //       "name": "R2-D2",
+      //       "height": "96",
+      //       "mass": "32",
+      //       "hair_color": "n/a",
+      //       "skin_color": "white, blue",
+      //       "eye_color": "red",
+      //       "birth_year": "33BBY",
+      //       "gender": "n/a",
+      //       "homeworld": "https://swapi.co/api/planets/8/",
+      //       "films": [
+      //          "https://swapi.co/api/films/2/",
+      //          "https://swapi.co/api/films/5/",
+      //          "https://swapi.co/api/films/4/",
+      //          "https://swapi.co/api/films/6/",
+      //          "https://swapi.co/api/films/3/",
+      //          "https://swapi.co/api/films/1/",
+      //          "https://swapi.co/api/films/7/"
+      //       ],
+      //       "species": [
+      //          "https://swapi.co/api/species/2/"
+      //       ],
+      //       "vehicles": [],
+      //       "starships": [],
+      //       "created": "2014-12-10T15:11:50.376000Z",
+      //       "edited": "2014-12-20T21:17:50.311000Z",
+      //       "url": "https://swapi.co/api/people/3/"
+      //    }
+      // ]);
    }, []);
 
-   if (characters) console.log(characters);
    return (
       <div className="App">
          <h1 className="Header">React Wars</h1>
          {
             (characters)
-            ?  <CharCard character={characters[0]} />
+            ?  <div className="cards">
+                  {characters.map(char => <CharCard character={char} />)}
+               </div>
             :  <div className="loading">
-                  <Spinner type="grow" color="secondary" />
+                  <Spinner color="warning" />
                </div>
          }
       </div>
--- starwars/src/components/CharCard.js
+++ starwars/src/components/CharCard.js
@@ -1,21 +1,36 @@
 import React from "react";
 import { Collapse, Card, CardHeader, CardImg, CardTitle, CardText, CardBody } from "reactstrap";
+import styled from "styled-components";
+
+const StyledCard = styled(Card)`
+   width: 300px;
+   margin-bottom: 2rem;
+   box-shadow: 0px 5px 10px 2px #81755c;
+`;
+
+const StyledHeader = styled(CardHeader)`
+   display: flex;
+   justify-content: space-around;
+   align-items: center;
+`;
 
 export default function CharCard ({character}) {
    return (
-      <Card>
-         <CardHeader>
+      <StyledCard>
+         <StyledHeader>
             {
                (character.gender === "male")
-               ?  <i className="fas fa-male fa-3x"></i>
-               :  <i className="fas fa-female fa-3x"></i>
-            }
+               ?  <i className="fas fa-male fa-2x"></i>
+               :  (character.gender === "female")
+                  ?  <i className="fas fa-female fa-2x"></i>
+                  :  <i className="fas fa-user-alt-slash fa-2x"></i>
+               }
             {character.name}
-         </CardHeader>
+         </StyledHeader>
 
          <CardBody>
-            It is pushing the envelope At the end of the FirstSpriti Digital Experience Platform powers enterprise-class. Spriti introduced new capabilities of the FirstSpriti Digital Experience Platform. Clicking on this link which refers to B2B Marketing awards shortlist will take you to the awards page of the customer journey.
+            --- Display Stats ---
          </CardBody>
-      </Card>
+      </StyledCard>
    );
 };
